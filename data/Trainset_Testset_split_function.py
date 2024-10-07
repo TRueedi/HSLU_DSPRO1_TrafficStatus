757551cc4121aeca83a66aa79097d0e2290da1c7
Samuel Paul <85477209+Samutheman@users.noreply.github.com>
2024-10-07 13:55:34 +0200

    [TASK] Cleaning and commeting of the function

--- data/Trainset_Testset_split_function.py
+++ data/Trainset_Testset_split_function.py
@@ -22,17 +22,24 @@ def split_data(df):
         test_set = pd.concat([test_set, group.loc[test_indices]])
         train_set = pd.concat([train_set, group.drop(test_indices)])
     return train_set, test_set
-
+#Path to the file from which the data is loaded
 pathFrom = r"C:\Users\samue\OneDrive\AIML\HS2024\Data Sicence Projekt\Data\London_UTD19.csv"
-pathTo = "C:\Users\samue\OneDrive\AIML\HS2024\Data Sicence Projekt\Data"
+#Path to the folder where the data is saved
+pathTo = r"C:\Users\samue\OneDrive\AIML\HS2024\Data Sicence Projekt\Data"
+#Number of splits. keep in mind that it could take a while to split the data 5 min per split
+numberOfSplits = 5
+print("Loading data from: ", pathFrom)
 dataLondonUTD19 = loadData(path=pathFrom)
-
 dataframeLondonUTD19 = pd.DataFrame(dataLondonUTD19)
-for i in range(5):
+print("Data loaded")
+print("Splitting data into ", numberOfSplits, " splits")
+for i in range(numberOfSplits):
+    print("Splitting ", i)
     train_set, test_set = split_data(dataframeLondonUTD19)
     df_test = pd.DataFrame(test_set)
     df_train = pd.DataFrame(train_set)
-    df_test.to_csv(pathTo+"\London_UTD19_test_"+i+".csv", index=False)
+    print("Saving split ", i, " to: ", pathTo)
+    df_test.to_csv(pathTo+"\\London_UTD19_test_"+i+".csv", index=False)
     df_train.to_csv(pathTo+"\\London_UTD19_train_"+i+".csv", index=False)
     print("Split ", i, " done")
 
