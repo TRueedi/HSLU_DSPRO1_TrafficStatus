22483cf45374c9303c908efcf3beca1bd7f8e035
Finn Eyer <finn.eyer24@gmail.com>
2024-11-01 17:47:48 +0100

    [TASK] Created a rfr model (random parameters, not finetuned) for first demo. Wrote function to train a model for each sensor and a function to predict the traffic for a certain weekday and a certain interval.

---
+++ model/rfr_model.py
@@ -0,0 +1,65 @@
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import GridSearchCV
+from sklearn.preprocessing import LabelEncoder
+import pandas as pd
+
+def train_rfr_models(train_data): 
+    weekday_mapping = {
+        'Monday': 0,
+        'Tuesday': 1,
+        'Wednesday': 2,
+        'Thursday': 3,
+        'Friday': 4,
+        'Saturday': 5,
+        'Sunday': 6
+    }
+    
+    param_grid = {
+        'n_estimators': [100, 200, 300],
+        'max_depth': [10, 20, 30],
+        'min_samples_split': [2, 5, 10],
+        'min_samples_leaf': [1, 2, 4]
+    }
+    
+    train_data['weekday'] = train_data['weekday'].map(weekday_mapping)
+    sensor_models = {}
+        
+    for sensor, sensor_data in train_data.groupby('detid'):
+        sensor_data = sensor_data.drop('detid', axis=1)
+        X_train = sensor_data.drop('traffic', axis=1)
+        y_train = sensor_data['traffic']
+        
+        rfr = RandomForestRegressor(random_state=27)
+        rfr_cv = GridSearchCV(estimator=rfr, param_grid=param_grid, cv=3, n_jobs=-1)
+        rfr_cv.fit(X_train, y_train)
+        
+        sensor_models[sensor] = rfr_cv
+    
+    return sensor_models
+    
+    
+def get_rfr_prediction(rfr_models, weekday, interval_values=
+               [0, 3600, 7200,10800, 14400, 18000, 21600, 25200, 28800, 32400, 36000, 39600, 43200, 46800, 50400, 54000, 57600, 61200, 64800, 68400, 72000, 75600, 79200, 82800]):
+    
+    X_values = pd.DataFrame(interval_values, columns=['interval'])
+    X_values['weekday'] = weekday
+    
+    predictions = []
+    
+    for sensor, sensor_model in rfr_models.items():
+        
+        y_pred = sensor_model.predict(X_values)
+        predictions.append(pd.DataFrame({
+            'traffic': y_pred,
+            'detid': sensor,
+            'interval': X_values['interval'],
+        }))
+        
+    return pd.concat(predictions)
+
+
+
+        
+        
+        
+    
