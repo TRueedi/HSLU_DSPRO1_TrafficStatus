eca360b5edd12ff1835c95b7a66266078fd20073
Finn Eyer <finn.eyer24@gmail.com>
2024-11-08 15:38:09 +0100

    [TASK] Added method to train one 4 dimensional model instead of a 3 dimensional model for each sensor.

--- model/rfr_model.py
+++ model/rfr_model.py
@@ -1,5 +1,6 @@
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import GridSearchCV
+from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 import joblib
 import pandas as pd
@@ -40,6 +41,42 @@ def train_rfr_models(train_data, save_path):
         joblib.dump(rfr_cv, model_path)
     
     
+def train_rfr_4d(train_data, save_path): 
+    weekday_mapping = {
+        'Monday': 0,
+        'Tuesday': 1,
+        'Wednesday': 2,
+        'Thursday': 3,
+        'Friday': 4,
+        'Saturday': 5,
+        'Sunday': 6
+    }
+    
+    param_grid = {
+        'n_estimators': [100, 200, 300, 400],
+        'max_depth': [10, 20, 30, 40],
+        'min_samples_split': [2, 5, 10],
+        'min_samples_leaf': [1, 2, 4]
+    }
+    
+    le = LabelEncoder()
+    
+    train_data['weekday'] = train_data['weekday'].map(weekday_mapping)
+    train_data['detid'] = le.fit_transform(train_data['detid'])
+        
+    # for sensor, sensor_data in train_data.groupby('detid'):
+        # sensor_data = sensor_data.drop('detid', axis=1)
+    X_train = train_data.drop('traffic', axis=1)
+    y_train = train_data['traffic']
+    
+    rfr = RandomForestRegressor(random_state=27)
+    rfr_cv = GridSearchCV(estimator=rfr, param_grid=param_grid, cv=5, n_jobs=-1, scoring='neg_mean_absolute_error')
+    rfr_cv.fit(X_train, y_train)
+    
+    model_path = f'{save_path}/rfr_4d_model'
+    joblib.dump(rfr_cv, model_path)    
+    
+
 def get_rfr_prediction(models_path, weekday, interval_values=
                [0, 3600, 7200,10800, 14400, 18000, 21600, 25200, 28800, 32400, 36000, 39600, 43200, 46800, 50400, 54000, 57600, 61200, 64800, 68400, 72000, 75600, 79200, 82800]):
     
